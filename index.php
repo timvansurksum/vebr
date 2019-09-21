4e122227cb23452ef197347386d24a0d847812fa
tim van surksum <you@example.com>
2019-09-21 15:18:18 +0200

    v5

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     echo "bye git world!"
-    echo "dit is text na v1"
-    echo "dit is text na v2"
-    echo "dit is text na v3"
+    echo "dit is text na v1";
+    echo "dit is text na v2";
+    echo "dit is text na v3";
+    echo "dit is text na v4";
 ?>
